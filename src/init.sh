3aa4ed67a226ab7b22f79f7c0bcb82660b98ab65
haroun <hash11223344@gmail.com>
2024-03-21 12:20:32 +0000

    testing playing spotify in realtime

--- src/init.sh
+++ src/init.sh
@@ -2,7 +2,7 @@
 
 #This init file is used to configure the input audio source and output audio source. The file can be edited to change the volume.
 
-pactl set-default-sink bluez_sink.60_AB_D2_57_42_A9.a2dp_sink
-pactl set-default-source alsa_input.usb-GeneralPlus_USB_Audio_Device-00.mono-fallback
-pactl set-source-volume alsa_input.usb-GeneralPlus_USB_Audio_Device-00.mono-fallback 16000
+pactl set-default-sink alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.analog-stereo
+pactl set-default-source alsa_input.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.mono-fallback
+pactl set-source-volume alsa_input.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.mono-fallback 16000
 pactl load-module module-loopback latency_msec=1
